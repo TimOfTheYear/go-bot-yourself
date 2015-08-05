78db0f8da1c896d5361b6c557c171b6071e46fa6
Tim Russo <tim.russo@realart.com>
2015-08-05 11:03:50 -0400

    Separated markov chain logic into its own reusable class.

--- bot.js
+++ bot.js
@@ -4,15 +4,20 @@ var Twit = require('twit');
 // We need to include our configuration file
 var T = new Twit(require('./config.js'));
 
-// This will be our Markov Chain object created during each learning.
-var markovChain = {};
+// This will be our Markov Chain prototype
+var MarkovChain = require('./markov.js');
 
 // Last tweet we responded too, only want mentions since then.
 var lastResponded;
 
-// What should be considered the end of a sentence?
-var punctuationFilter = [ ".", "!", "?" ];
-
+/**
+ * Fetches a user's timeline, then constructs a Markov chain from
+ * their status history.
+ *
+ * @param username - The twitter screen_name whose history should be fetched.
+ * @param onComplete - Callback function where the completed markov chain will
+ *          will be return to.
+ */
 function learnFromUser(username, onComplete) {
     T.get("statuses/user_timeline", { screen_name: username, count: 200,
         exclude_replies: true, include_rts: false}, function (error, data) {
@@ -20,88 +25,25 @@ function learnFromUser(username, onComplete) {
                 var tweets = [];
 
                 console.log("Tweets returned: ", data.length);
+                var markovChain = new MarkovChain();
 
                 for(var i = 0; i < data.length; i++) {
-                    learnFromTweet(data[i].text);
+                    markovChain.learnFromSource(data[i].text);
                 }
 
                 console.log("Learning Complete, Markov Chain: ", markovChain);
 
-                onComplete();
+                onComplete(markovChain);
             } else {
                 console.log("Error: ", error);
             }
         });
 }
 
-function learnFromTweet(tweet) {
-    var pieces = tweet.split(" ");
-    var lastWord = "";
-
-    for(var i = 0; i < pieces.length; i++) {
-        var punctuation = [];
-        var endChar = pieces[i].charAt(pieces[i].length - 1);
-
-        while(punctuationFilter.indexOf(endChar) >= 0) {
-            punctuation.push(endChar);
-            pieces[i] = pieces[i].substring(0, pieces[i].length - 1);
-            endChar = pieces[i].charAt(pieces[i].length - 1);
-        }
-
-        if (typeof markovChain[lastWord] == 'undefined') {
-            markovChain[lastWord] = {};
-            markovChain[lastWord][pieces[i]] = 0;
-        } else if (typeof markovChain[lastWord][pieces[i]] == 'undefined') {
-            markovChain[lastWord][pieces[i]] = 0;
-        }
-
-        markovChain[lastWord][pieces[i]]++;
-        lastWord = pieces[i];
-
-        if (typeof markovChain[lastWord] == 'undefined') {
-            markovChain[lastWord] = {};
-        }
-
-        for(var p = 0; p < punctuation.length; p++) {
-            if(typeof markovChain[lastWord][punctuation[p]] == 'undefined') {
-                markovChain[lastWord][punctuation[p]] = 0;
-            }
-
-            markovChain[lastWord][punctuation[p]]++;
-        }
-    }
-}
-
-function generateTweet() {
-    var tweet = "";
-    var lastWord = "";
-
-    while(tweet.length < 120 && punctuationFilter.indexOf(tweet.charAt(tweet.length - 1)) < 0 &&
-        Object.keys(markovChain[lastWord]).length > 0) {
-        var potentials = [];
-
-        // Create a potentials array with one entry per frequency in tweets. There has got to be
-        // a better way to do this rather than duplicating array entries, but I haven't figured
-        // it out yet. ¯\_(ツ)_/¯
-        for(var key in markovChain[lastWord]) {
-            for(var i = 0; i < markovChain[lastWord][key]; i++) {
-                potentials.push(key);
-            }
-        }
-
-        lastWord = potentials[Math.floor(Math.random() * potentials.length)];
-
-        // If its not the beginning or end of a sentence, add a space.
-        if(punctuationFilter.indexOf(lastWord) < 0 && tweet != "") {
-            tweet += " ";
-        }
-
-        tweet += lastWord;
-    }
-
-    return tweet;
-}
-
+/**
+ * Fetches the authenticated twitter accounts mentions feed issues a response
+ * to the most recent mentioner.
+ */
 function checkForRequests() {
     var params = {};
 
@@ -113,8 +55,8 @@ function checkForRequests() {
         if(!error) {
             if(data.length > 0) {
                 // TODO : Reply to multiple people, up to a maximum of 12 / minute
-                learnFromUser(data[0].user.screen_name, function() {
-                    T.post("statuses/update", { status:'"' + generateTweet() + '" - @' + data[0].user.screen_name,
+                learnFromUser(data[0].user.screen_name, function(chain) {
+                    T.post("statuses/update", { status:'"' + chain.generateString(120) + '" - @' + data[0].user.screen_name,
                         in_reply_to_status_id: data[0].id_str }, function(error, body, response) {
                             if(!error) {
                                 lastResponded = data[0].id_str;
